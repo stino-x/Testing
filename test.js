0f85d82e46ffb6dbd6e83ea90e30e3d999268538
stino-x <107426247+stino-x@users.noreply.github.com>
2023-03-11 17:45:55 +0100

    this adds the functionality of having at least one character

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ function stringLength(string) {
   let count = 0;
   for (let i = 0; i < string.length; i += 1) {
     count += 1;
-  }
+  };
   return count;
 }
 function stringLengthTest(string) {
@@ -16,6 +16,8 @@ try {
 
   if (expected !== result) {
     throw new Error(`${result} is not equal to ${expected}`);
+  } else if(stringLength('') === 0){
+    throw new Error(`no characters detected`);
   } else {
     return `${result} is equal to ${expected}`;
   }
